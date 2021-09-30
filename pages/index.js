e94a686c23401e2b9bd8f60ca465826d64b63209
Alex Munayyer <alexmunayyer@gmail.com>
2021-09-29 20:12:45 -0700

    Trying to get this to work

--- pages/index.js
+++ pages/index.js
@@ -306,6 +306,17 @@ export default function Index() {
         </section>
       </div>
       <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
+      <Script>
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on("init", user => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          })
+        }
+      </Script>
     </div>
   )
 }
