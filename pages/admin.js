81f83d4ec700015c37e67d364ef6dfd332a974aa
Alex Munayyer <alexmunayyer@gmail.com>
2021-09-30 11:36:16 -0700

    Lets see if this works

--- pages/admin.js
+++ pages/admin.js
@@ -1,11 +1,13 @@
 import { useEffect } from 'react'
-//import BlogPostPreview from '../previews/BlogPostPreview'
+import homePagePreview from '../pages/index'
 
 const Admin = () => {
     useEffect(() => {
         ; (async () => {
             const CMS = (await import('netlify-cms-app')).default
             CMS.init()
+            // hook our preview up to the cms
+            CMS.registerPreviewTemplate('Home Page Content', homePagePreview)
         })()
     }, [])
 
